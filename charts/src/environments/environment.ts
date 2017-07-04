eb50a11f9ba976c6be2270672e5dd574b8ad35f5
smoore2386 <smoore2386@gmail.com>
2017-07-04 14:48:36 -0400

    missed a comma

--- charts/src/environments/environment.ts
+++ charts/src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
   firebase: {
     apiKey: '<your-key>',
     authDomain: '<your-project-authdomain>',
